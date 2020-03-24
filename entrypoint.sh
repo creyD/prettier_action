65e822b2f3297ffb251f68e0470b0224a54ab85d
Conrad <grosserconrad@gmail.com>
2020-03-24 09:56:43 +0100

    New Parameter Defaults To Null

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,7 +37,7 @@ _git_push() {
 
 # PROGRAM
 echo "Installing prettier..."
-if ! "$INPUT_PRETTIER_VERSION"; then
+if -z "$INPUT_PRETTIER_VERSION"; then
   npm install --silent --global prettier
 else
   npm install --silent --global prettier@$INPUT_PRETTIER_VERSION
