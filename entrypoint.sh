e338a630e679fac7089c57b02995c9e8942a19e6
Josh Soref <jsoref@users.noreply.github.com>
2021-02-19 00:49:16 -0500

    Tag entrypoint as a bash script
    This script uses bashisms

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # e is for exiting the script automatically if a command fails, u is for exiting if a variable is not set
 # x would be for showing the commands before they are executed
 set -eu
