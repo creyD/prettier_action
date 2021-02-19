5e56c643886c64170505a86fa95a03744b6f6bd2
creyD <grosserconrad@gmail.com>
2021-02-19 19:13:29 +0100

    Another try at bugfixing the ignore mechanism

--- entrypoint.sh
+++ entrypoint.sh
@@ -60,8 +60,8 @@ echo "Files:"
 prettier $INPUT_PRETTIER_OPTIONS || echo "Problem running prettier with $INPUT_PRETTIER_OPTIONS"
 
 # Ignore node modules and other action created files
-git update-index --skip-worktree package_lock.json
-git update-index --skip-worktree node_modules/
+rm -r node_modules/
+git reset --hard package-lock.json || rm package-lock.json
 
 # To keep runtime good, just continue if something was changed
 if _git_changed; then
